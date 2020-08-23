ce5d968cfaffcf94df7ae8406986b52fd1e42543
hisan <13821823961@163.com>
2020-08-23 17:32:25 +0800

    info

--- 30/back_30.c
+++ 30/back_30.c
@@ -1,4 +1,4 @@
-//2020-02-24 20:30 超时
+//2020-08-23 14:16 超时
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,172 +7,137 @@
 #define  NOT_MATCH  0
 #define  YES_MATCH  1
 
-#define USE_NOT    0
-#define USE_YES    1
-
-int matchWords(int *flagArray,char	*MasterString,char **words,int wordsSize,int *returnSize,int *times)
+int matchleftwords(char *curstart,char ** words, int wordsSize,int *used,int *count,char *S,int *savepos)
 {
-	int i = 0;
-	char *pMaster = MasterString;
-	char *pSub = NULL;
-	
-	for (i = 0 ; i < wordsSize;i++)
-	{
-		if (flagArray[i] == USE_YES)
-		{
-			continue;
-		}
+    char *nextstart = NULL;
+    
+    if (*count == wordsSize)
+    {//已构成
+        return YES_MATCH;
+    }
+
+    for (int i = 0 ; i < wordsSize;i++)
+    {
+        if (used[i] == 1)
+        {
+            continue;
+        }
+
+        if (NULL == (nextstart = strstr(curstart,words[i])))
+        {
+            continue;
+        }
+
+        if (nextstart != curstart)//当前字符不紧挨着，则跳到下一个字符串继续比对
+        {
+            continue;
+        }
+
+        used[i] = 1;
+		*count += 1;
 		
-		if (NULL != (pSub = strstr(MasterString,words[i])))
-		{
-			if (pSub == pMaster)//比较地址
-			{
-				flagArray[i] = USE_YES;
-				pMaster += strlen(words[i]);
-				(*times) += 1;
-				
-				if ((*times) == wordsSize)//完全匹配
-				{
-					(*returnSize) += 1;
-					return YES_MATCH;
-				}
-				else 
-				{
-					return matchWords(flagArray,pMaster,words,wordsSize,returnSize,times);
-				}
-			}
-		}
-		else 
-		{
-			return NOT_MATCH;
-		}
-	}
+        curstart += strlen(words[i]);
+        //只要匹配了就返回，不需要得到所有符合条件的情况。
+        if ( YES_MATCH == matchleftwords(curstart,words,wordsSize,used,count,S,savepos))
+        {
+            return YES_MATCH;
+        }
+    }
 	
-	return NOT_MATCH;
+    return NOT_MATCH;
 }
 
-int* findSubstring(char * s, char ** words, int wordsSize, int* returnSize)
-{    
-	char *pstr = NULL;
-    int  i = 0,j = 0,l = 0;
-	int sublen = 0;
-	int *usedflag = NULL;
-	int *retArray = NULL;
-	int retSize = 0;
-	int match_size = 0;
-	int match_times = 0;
-	char *ptmp = NULL;
-	
-	int index = 0;
-	int alreay = 0;
-	int len = 0;
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findSubstring(char * s, char ** words, int wordsSize, int* returnSize){
 
-    int masterlen = strlen(s);
-	
+    int *used = (int*)malloc(sizeof(int)*wordsSize);
+    int *savepos = NULL;
+	int count = 0;
+    char *curstart = NULL;
+    char *nextstart = NULL;
 	*returnSize = 0;
 	
-	if (wordsSize == 1)
-	{
-		if (0 == strcmp(s,words[0]))
-		{
-			retArray = realloc(retArray,sizeof(int)*(match_size+1));
-			retArray[match_size] = 0;
-			(*returnSize) += 1;
-		}
-		else 
-		{
-			pstr = strstr(s,words[0]);
-			while (pstr != NULL)
-			{
-				index = masterlen - strlen(pstr);
-				retArray = realloc(retArray,sizeof(int)*(match_size+1));
-				retArray[match_size] = index;
-				match_size++;
-				(*returnSize) += 1;
-				pstr++;
-				pstr = strstr(pstr,words[0]);
-			}
-		}
-		return retArray;
-	}
+	int len = strlen(s);
+	int sublen = 0;
 	
-	usedflag = (int *)malloc(sizeof(int)*wordsSize);
-	if (usedflag == NULL)
+	for (int i = 0 ; i < wordsSize;i++)
 	{
-		return NULL;
-	}
-
-	for (i = 0; i < wordsSize;i++)
-	{//得到子串长度
 		sublen += strlen(words[i]);
 	}
 	
-	len = strlen(s) - sublen + 1;
+	len -= sublen;
 	
-	//匹配策略：分别以每个单词作为组合后的子串的开头去进行匹配。
-	for (i = 0;i < wordsSize;i++)
+	for (int j = 0; j <=len;j++)
 	{
-		masterlen = strlen(s);
-		pstr = s;
-		while ( (pstr = strstr(pstr,words[i])) != NULL && strlen(pstr) >= sublen )//在当前单词能够匹配到主串中的一部分时，则要保证主串s中以当前单词开头至尾部的长度要大于等于所有单词的长度之和(待匹配词组构成的子串的长度之和)。
+		for (int i = 0 ; i < wordsSize;i++)
 		{
-			memset(usedflag,USE_NOT,sizeof(int)*wordsSize);
-			match_times = 0;
-			usedflag[i] = USE_YES;
-			match_times += 1;
+			nextstart = NULL;
+			curstart = s + j;
+			count = 0;
 			
-			ptmp = pstr;
-			ptmp += strlen(words[i]);
-			if (NOT_MATCH == matchWords(usedflag,ptmp,words,wordsSize,&retSize,&match_times))
+			memset(used,0,sizeof(int)*wordsSize);//只保存本次深度优先遍历的状态，开始下一次深度之前要清除上一次的状态
+			
+			if ((nextstart = strstr(curstart,words[i])) == NULL)//当前子单词不可能组成字符串
 			{
-				pstr += 1;
 				continue;
 			}
-			else 
+			
+			curstart = nextstart + strlen(words[i]);
+			used[i] = 1;
+			count += 1;
+			
+			if ( YES_MATCH == matchleftwords(curstart,words,wordsSize,used,&count,s,savepos))
 			{
-				index = masterlen - strlen(pstr);
-				alreay = 0;
-				for (j = 0;j < match_size;j++)
+				int tmplen = strlen(s) - strlen(nextstart);
+				int already = 0;
+				
+				for (int k = 0 ; k < *returnSize;k++)
 				{
-					if (retArray[j] == index)
+					if (savepos[k] == tmplen)
 					{
-						alreay = 1;
+						already = 1;
 						break;
 					}
 				}
 				
-				if (alreay != 1)
+				if (already == 1)
 				{
-					retArray = realloc(retArray,sizeof(int)*(match_size+1));
-					retArray[match_size] = index;
-					match_size++;
+					continue;
 				}
-				else
-				{
-					retSize -= 1;
-				}
-				pstr += 1;
+			
+				savepos = realloc(savepos,sizeof(int)*(*returnSize + 1));
+				savepos[*returnSize] = strlen(s) - strlen(nextstart);
+				*returnSize += 1;
 			}
 		}
 	}
 	
-	*returnSize = retSize;
-	return retArray;
+    return savepos;
 }
-
 
 int main()
 {
-	//char *s = "wordgoodgoodgoodbestword";	
-	//char *words[] = {"word","good","best","word"};
-	//char *s = "barfoothefoobarman";
-	//char *words[] = {"foo","bar"};
+	#if 0
+	char *s = "wordgoodgoodgoodbestword";	
+	char *words[] = {"word","good","best","word"};
+	#endif 
+	
+	#if 0
+	char *s = "barfoobarthefoobarman";
+	char *words[] = {"foo","bar"};
+	#endif 
+	
+	#if 0
+	char *s = "aabbaabbaabb";
+	char *words = {"bb","aa","bb","aa","bb"};
+	#endif 
 	
 	char *s = "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababab";
-
 	char *words[] = {"ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba","ab","ba"};
 	
-
+	
 	int wordsSize = sizeof(words)/sizeof(char *);
 	int retSize = 0;
 	int i = 0;
@@ -181,10 +146,10 @@ int main()
 	int *ret = findSubstring(s, words, wordsSize, &retSize);
 	printf("after:%d\r\n",time(NULL));
 	
-	if (retSize <= 0)
+	if (ret == NULL || retSize <= 0 )
 	{
 		printf("no match!\r\n");
-		return 0;
+		return NOT_MATCH;
 	}
 	
 	for (i = 0; i < retSize-1;i++)
@@ -193,24 +158,7 @@ int main()
 	}
 	
 	printf("%d\r\n",ret[retSize - 1]);
-	return 0;
+	
+	return YES_MATCH;
 }
-
-
-/*
-执行耗时：单位s
-
-before:1598174152
-after: 1598174280
-
-共计：128s 
-
-垃圾算法!
-
-no match!
-
-*/
-
-
-
 
