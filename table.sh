403c50965f7183182d5e2b4f42d935de9e6dda66
zyb <13821823961@163.com>
2021-03-29 17:55:04 +0800

    2021-03-29 17:55:04

--- table.sh
+++ table.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 rm readme.md
-echo '
-2020 02-12 17:36 浙江杭州
-上传以前刷过的leetcode的题目以及思考
-以及现在或不久的刷题思路和代码
-' >> readme.md
-echo ""
-echo ""
+echo '2020 02-12 17:36 浙江杭州' >> readme.md
+echo "上传刷过的leetcode的题目以及思考" >> readme.md
+echo "" >> readme.md
+echo "" >> readme.md
 
 echo "题号 |链接|实现语言" >> readme.md
 echo "-|-|-" >> readme.md
