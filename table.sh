bd0bc1626c55085a187c0769e06b12659192b4f2
zyb <13821823961@163.com>
2021-03-29 14:23:36 +0800

    2021-03-29 14:23:36

--- table.sh
+++ table.sh
@@ -9,6 +9,6 @@ echo ""
 echo ""
 
 echo "题号 |链接|实现语言" >> readme.md
-echo ""
+echo "-|-|-"
 
 ls | sort -n | awk 'BEGIN{pre="https://github.com/hisan/myleetcode/tree/master/"}{print $1" | "pre$1" | c"}' >> readme.md
