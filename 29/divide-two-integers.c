eb3efec9e3a04ce97d3120861ba6dc921c4e1cd2
zyb <13821823961@163.com>
2021-04-08 23:00:27 +0800

    2021-04-08 23:00:27

--- 29/divide-two-integers.c
+++ 29/divide-two-integers.c
@@ -6,39 +6,37 @@
 #define INT_MIN -2147483648
 
 int divide(int dividend, int divisor){
-    int res = 0;
+    int sign = (dividend<0)^(divisor<0)==0?1:-1;
+    if (dividend == INT_MIN && divisor == -1)
+    {
+        return INT_MAX;
+    }
+
     if (dividend == 0)
     {
         return 0;
     }
 
-    if (dividend == INT_MIN && divisor == -1)//此时dividend/divisor = INT_MAX+1
+    if (divisor == 1)
     {
-        return INT_MAX;
+        return dividend;
     }
-
-    int sign = (dividend < 0)^(divisor < 0) == 1?-1:1;//同为0，异为1,判断除数与被除数符号情况
-    
-    long m = labs(dividend);
-    long n = labs(divisor);
-    
-    if (n == 1)
+    long div = labs(dividend);
+    long divor = labs(divisor);
+    long res = 0;
+    while (div >= divor)
     {
-        return sign == 1?-m:m;
-    }
-
-     while (m >= n) 
-     {
-        long t = n, p = 1;
-        while (m >= (t << 1)) {
-            t <<= 1;
-            p <<= 1;
+        long tmp = divor;
+        long p = 1;
+        while (div >= (tmp<<1))
+        {
+            tmp<<=1;
+            p<<=1;
         }
         res += p;
-        m -= t;
+        div -= tmp;
     }
-
-    return sign == 1 ? res : -res;
+    return sign==1?res:-res;
 }
 
 int main()
